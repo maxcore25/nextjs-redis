8a06621b25a2def1e6924f0476c00498837e8057
maxcore25 <mkorobov2002@mail.ru>
2022-02-20 00:32:51 +0300

    Various Images Testing passed and Small Bugs fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: ['img.autoum.com', 'mir-s3-cdn-cf.behance.net', 'carsweek.ru'],
+  },
+};
