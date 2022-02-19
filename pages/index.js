335c14295d59a978ef16692fa203fd0bed8c8feb
maxcore25 <mkorobov2002@mail.ru>
2022-02-19 23:40:10 +0300

    Search Form Testing succeeded

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import CarForm from '../lib/CarForm';
+import SearchForm from '../lib/SearchForm';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <CarForm />
+      <SearchForm />
+      {/* <CarForm /> */}
     </div>
   );
 }
